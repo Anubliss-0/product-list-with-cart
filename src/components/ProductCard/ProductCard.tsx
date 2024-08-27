fb050f7b6217009b75c76e532cdb1b7893d1944e
Thomas Rice <59601109+Anubliss-0@users.noreply.github.com>
2024-08-27 19:09:06 +0200

    app.tsx layout styled down to mobile

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -58,7 +58,7 @@ function ProductCard({ foodItem, section }: ProductCardProps) {
     return (
         <article className={styles.productCard}>
             <picture>
-                <source media="(max-width: 375px)" srcSet={foodItem.image.mobile} />
+                <source media="(max-width: 530px)" srcSet={foodItem.image.mobile} />
                 <source media="(max-width: 768px)" srcSet={foodItem.image.tablet} />
                 <source media="(min-width: 769px)" srcSet={foodItem.image.desktop} />
                 <img src="./assets/images/image-waffle-desktop.jpg" alt={t(foodItem.name)} />
