215acad60994b86a5321276d571f52b21001728d
Thomas Rice <59601109+Anubliss-0@users.noreply.github.com>
2024-09-04 23:48:20 +0200

    Add translation keys for other languages

--- src/i18n.ts
+++ src/i18n.ts
@@ -104,7 +104,157 @@ i18n
                         "increaseQuantity": "Hoeveelheid verhogen"
                     }
                 }
-            }
+            },
+            fr: {
+                translation: {
+                    "addToCart": "Ajouter au panier",
+                    "carbonNeutral": "Ceci est une livraison <1>neutre en carbone</1>",
+                    "starters": {
+                        "title": "Entrées",
+                        "category": {
+                            "appetizer": "Apéritif",
+                            "bread": "Pain",
+                            "salad": "Salade"
+                        },
+                        "items": {
+                            "bruschetta": "Bruschetta",
+                            "stuffedMushrooms": "Champignons farcis",
+                            "garlicBreadsticks": "Gressins à l'ail",
+                            "capreseSalad": "Salade Caprese",
+                            "miniQuiches": "Mini quiches"
+                        }
+                    },
+                    "desserts": {
+                        "title": "Desserts",
+                        "category": {
+                            "waffle": "Gaufre",
+                            "cremeBrulee": "Crème brûlée",
+                            "macaron": "Macaron",
+                            "tiramisu": "Tiramisu",
+                            "baklava": "Baklava",
+                            "cake": "Gâteau",
+                            "brownie": "Brownie",
+                            "pannaCotta": "Panna cotta",
+                            "pie": "Tarte"
+                        },
+                        "items": {
+                            "waffleWithBerries": "Gaufre aux fruits rouges",
+                            "vanillaBeanCremeBrulee": "Crème brûlée à la vanille",
+                            "macaronMixofFive": "Assortiment de cinq macarons",
+                            "classicTiramisu": "Tiramisu classique",
+                            "pistachioBaklava": "Baklava à la pistache",
+                            "lemonMeringuePie": "Tarte au citron meringuée",
+                            "redVelvetCake": "Gâteau Red Velvet",
+                            "saltedCaramelBrownie": "Brownie au caramel salé",
+                            "vanillaPannaCotta": "Panna cotta à la vanille"
+                        }
+                    },
+                    "ariaLabels": {
+                        "decreaseQuantity": "Diminuer la quantité",
+                        "increaseQuantity": "Augmenter la quantité"
+                    }
+                }
+            },
+            es: {
+                translation: {
+                    "addToCart": "Añadir al carrito",
+                    "carbonNeutral": "Este es un envío <1>neutral en carbono</1>",
+                    "starters": {
+                        "title": "Entrantes",
+                        "category": {
+                            "appetizer": "Aperitivo",
+                            "bread": "Pan",
+                            "salad": "Ensalada"
+                        },
+                        "items": {
+                            "bruschetta": "Bruschetta",
+                            "stuffedMushrooms": "Champiñones rellenos",
+                            "garlicBreadsticks": "Palitos de pan con ajo",
+                            "capreseSalad": "Ensalada Caprese",
+                            "miniQuiches": "Mini quiches"
+                        }
+                    },
+                    "desserts": {
+                        "title": "Postres",
+                        "category": {
+                            "waffle": "Gofre",
+                            "cremeBrulee": "Crème brûlée",
+                            "macaron": "Macaron",
+                            "tiramisu": "Tiramisú",
+                            "baklava": "Baklava",
+                            "cake": "Pastel",
+                            "brownie": "Brownie",
+                            "pannaCotta": "Panna Cotta",
+                            "pie": "Tarta"
+                        },
+                        "items": {
+                            "waffleWithBerries": "Gofre con frutas del bosque",
+                            "vanillaBeanCremeBrulee": "Crème brûlée de vainilla",
+                            "macaronMixofFive": "Mezcla de cinco macarons",
+                            "classicTiramisu": "Tiramisú clásico",
+                            "pistachioBaklava": "Baklava de pistacho",
+                            "lemonMeringuePie": "Tarta de limón con merengue",
+                            "redVelvetCake": "Pastel de terciopelo rojo",
+                            "saltedCaramelBrownie": "Brownie de caramelo salado",
+                            "vanillaPannaCotta": "Panna cotta de vainilla"
+                        }
+                    },
+                    "ariaLabels": {
+                        "decreaseQuantity": "Disminuir cantidad",
+                        "increaseQuantity": "Aumentar cantidad"
+                    }
+                }
+            },
+            de: {
+                translation: {
+                    "addToCart": "In den Warenkorb",
+                    "carbonNeutral": "Dies ist eine <1>CO₂-neutrale</1> Lieferung",
+                    "starters": {
+                        "title": "Vorspeisen",
+                        "category": {
+                            "appetizer": "Vorspeise",
+                            "bread": "Brot",
+                            "salad": "Salat"
+                        },
+                        "items": {
+                            "bruschetta": "Bruschetta",
+                            "stuffedMushrooms": "Gefüllte Champignons",
+                            "garlicBreadsticks": "Knoblauchbrotstangen",
+                            "capreseSalad": "Caprese Salat",
+                            "miniQuiches": "Mini-Quiches"
+                        }
+                    },
+                    "desserts": {
+                        "title": "Desserts",
+                        "category": {
+                            "waffle": "Waffel",
+                            "cremeBrulee": "Crème Brûlée",
+                            "macaron": "Macaron",
+                            "tiramisu": "Tiramisu",
+                            "baklava": "Baklava",
+                            "cake": "Kuchen",
+                            "brownie": "Brownie",
+                            "pannaCotta": "Panna Cotta",
+                            "pie": "Torte"
+                        },
+                        "items": {
+                            "waffleWithBerries": "Waffel mit Beeren",
+                            "vanillaBeanCremeBrulee": "Vanille Crème Brûlée",
+                            "macaronMixofFive": "Macaron-Mix aus fünf",
+                            "classicTiramisu": "Klassisches Tiramisu",
+                            "pistachioBaklava": "Baklava mit Pistazien",
+                            "lemonMeringuePie": "Zitronen-Baiser-Torte",
+                            "redVelvetCake": "Red Velvet Kuchen",
+                            "saltedCaramelBrownie": "Brownie mit gesalzenem Karamell",
+                            "vanillaPannaCotta": "Vanille Panna Cotta"
+                        }
+                    },
+                    "ariaLabels": {
+                        "decreaseQuantity": "Menge verringern",
+                        "increaseQuantity": "Menge erhöhen"
+                    }
+                }
+            },
         },
         lng: "en",
         fallbackLng: "en",
